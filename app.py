b430eed929d1ea1ccc9dd859f220ebb49a4eb545
Khaliglearning <93950853+Khaliglearning@users.noreply.github.com>
2021-12-10 23:28:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
+app = dash.Dash(__name__)
+
+df = pd.read_csv("dados.csv")
+
+fig = px.bar(df, x="DT_MEDICAO", y="UMD_INS", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Umidade'),
+
+    html.Div(children='''
+        umidade do ar
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
